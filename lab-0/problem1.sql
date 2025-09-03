8289c8e4d9b9179f5828d5272df2cc3dd38c2375
Amit Yadav <amityadavv@outlook.in>
2025-09-03 10:44:27 +0530

    Update problem1.sql
    final submission of problem 1-- added comments

--- lab-0/problem1.sql
+++ lab-0/problem1.sql
@@ -1,3 +1,5 @@
+--create table employee
+
 create table employee(
     emp_no varchar2(10),
     emp_name varchar2(100),
@@ -10,14 +12,52 @@ create table employee(
 );
 describe employee;
 
-create table department(
+--create table department 
+
+create table Department(
     dept_id varchar2(10),
     d_name varchar2(80),
     d_location varchar2(50)
 );
-describe department;
+describe Department;
+
+-- add colomn to employee table 
+
 alter table employee
 add(
     emp_salary int,
     emp_bateofbirth date
 );
+describe employee;
+
+-- rename column  
+alter table employee
+rename column emp_bateofbirth to emp_dob;
+
+--deleting column 
+
+alter table employee
+drop column emp_dob;
+describe employee;
+
+--rename table name 
+
+rename Department to remDept;
+describe remDept;
+
+--rename column name
+
+alter table employee
+rename column emp_city to emp_loc;
+
+--view both the table
+
+describe employee;
+describe remDept;
+
+
+
+
+
+
+
