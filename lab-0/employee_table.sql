b0c63c5c1a39e482bcb5d4b63a1fa4aa00543ca3
Amit Yadav <amityadavv@outlook.in>
2025-09-03 10:01:20 +0530

    Create employee_table.sql
    Implementation of DDL commands (create, alter, drop and truncate)
    with and without using constraints.

---
+++ lab-0/employee_table.sql
@@ -0,0 +1,11 @@
+create table employee(
+    emp_no varchar2(10),
+    emp_name varchar2(100),
+    emp_street varchar2(100),
+    emp_city varchar2(50),
+    emp_job varchar2(50),
+    emp_hiredate date,
+    emp_mgr varchar2(10),
+    emp_deptno varchar2(10)    
+);
+describe employee;
